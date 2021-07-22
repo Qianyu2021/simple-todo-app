dcc4fcd237dcf8918650d4cd120f8c60c31bfb13
qianyu2021 <qyu14@horizon.csueastbay.edu>
2021-07-22 16:47:21 -0700

    first commit

---
+++ app/src/main/java/com/example/mytodolist/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.mytodolist;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import org.apache.commons.io.FileUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static com.example.mytodolist.itemAdapter.*;
+import static org.apache.commons.io.FileUtils.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String KEY_ITEM_TEXT = "item_text";
+    public static final String KEY_ITEM_POSITION = "item_position";
+    public static final int EDIT_TEXT_CODE = 20;
+
+    List<String> items = new ArrayList<>();
+    Button addButton;
+    EditText textEdit;
+    RecyclerView ryitem;
+    itemAdapter newAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addButton = findViewById(R.id.addButton);
+        textEdit = findViewById(R.id.textEdit);
+        ryitem = findViewById(R.id.ryitem);
+
+        loadItems();
+
+        itemAdapter.OnLongClickListener onLongClickListener = new itemAdapter.OnLongClickListener() {
+            @Override
+            public void onItemLongClicked(int position) {
+                //delete the item from the model
+                items.remove(position);
+                //notify the adapter
+               newAdapter.notifyItemInserted(position);
+               Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        };
+        itemAdapter.OnClickListener onClickListener = new itemAdapter.OnClickListener() {
+
+            @Override
+            public void onItemClicked(int position) {
+                Log.d("MainActivity", "Single click at position" + position);
+                //create the new activity
+                Intent i= new Intent(MainActivity.this, EditActivity.class
+                );
+                //pass the data being edited
+                i.putExtra(KEY_ITEM_TEXT, items.get(position));
+                i.putExtra(KEY_ITEM_POSITION, position);
+                //display the activity
+
+                ActivityResultLauncher<Intent> startForResultLauncher = registerForActivityResult(
+                        new ActivityResultContracts.StartActivityForResult(),
+                        new ActivityResultCallback<ActivityResult>() {
+                            @Override
+                            public void onActivityResult(ActivityResult result) {
+                                if(result.getResultCode() == Activity.RESULT_OK) {
+                                    Intent data = result.getData();
+                                    System.out.println(data);
+                                }
+                            }
+                        });
+                startForResultLauncher.launch(i);
+            }
+        };
+
+
+        newAdapter = new itemAdapter(items, onLongClickListener, onClickListener);
+        ryitem.setAdapter(newAdapter);
+        ryitem.setLayoutManager(new LinearLayoutManager(this));
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String todoItem = textEdit.getText().toString();
+               // add item to the model
+                items.add(todoItem);
+                //notify adapter that an item is inserted
+                newAdapter.notifyItemInserted(items.size()-1);
+                textEdit.setText("");
+                Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        });
+    }
+
+    // handle the result of the edit activity
+
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (resultCode == RESULT_OK && requestCode == EDIT_TEXT_CODE) {
+        //retrieve the updated text value
+        String itemText = data.getStringExtra(KEY_ITEM_TEXT);
+        //extract the original position of the edited item from the position key
+        int position = data.getExtras().getInt(KEY_ITEM_POSITION);
+
+        //update the model at the right position with new item text
+        items.set(position, itemText);
+        //notify the adapter
+        newAdapter.notifyItemChanged(position);
+        //persist the changes
+        saveItems();
+        Toast.makeText(getApplicationContext(), "item updated successfully", Toast.LENGTH_SHORT).show();
+    } else {
+        Log.w("MainActivity", "Unknown call to onActivityResult");
+    }
+}
+        private File getDataFile() {
+
+            return new File(getFilesDir(),"data.txt");
+        }
+        //this function will load items by reading line of the data file
+        private void loadItems(){
+        try {
+            items = new ArrayList<>(readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading items", e);
+            items = new ArrayList<>();
+          }
+        }
+
+    //this function saves items by writng them to the data file
+    private void saveItems(){
+        try {
+            writeLines(getDataFile(), items);
+        } catch (IOException e) {
+           Log.e("MainActivity", "Error writing items", e);
+          }
+    }
+}
